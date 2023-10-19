18ebde8fd456bfb07bcecd5858251787b024e42e
CianeB <147665823+CianeB@users.noreply.github.com>
2023-10-19 23:08:35 +0000

    novoprojeto

--- index1.js
+++ index1.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/test');
 
-const produto = mongoose.model('produto', {produto: String, codigo: Number, nome: String, preco: Number});
+// const produto = mongoose.model('produto', {produto: String, codigo: Number, nome: String, preco: Number});
 
 // const modeloDeProduto = new produto({
 //     produto:'biscoito', 
@@ -12,7 +12,41 @@ const produto = mongoose.model('produto', {produto: String, codigo: Number, nome
 // console.log("Salvou")
 
 
-async function exibeProdutos() {
-    console.log(await produto.find())
-}
-exibeProdutos()
+// async function exibeProdutos() {
+//     console.log(await produto.find())
+// }
+// exibeProdutos()
+
+async function main() {
+    const Produto = mongoose.model('Produto',
+    {
+        codigo: Number,
+        nome: String,
+        preco:Number,
+    })}
+
+    Produto.create({
+        codigo:32,
+        nome:'calca social',
+        preco:89,
+
+        codigo:33,
+        nome:'calca jeans',
+        preco:99,
+
+        codigo:34,
+        nome:'calca legging',
+        preco:59,
+
+        codigo:35,
+        nome:'calca sarja',
+        preco:79,
+    })
+
+    await Produto.findOneAndUpdate(
+        {preco:99,}
+        {preco:120}
+        )
+
+        await Produto.findOneAndDelete({preco:59})
+
