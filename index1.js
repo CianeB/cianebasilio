4fc3305363e871afaefb931dd2d4e1ec815f9568
CianeB <147665823+CianeB@users.noreply.github.com>
2023-10-20 23:05:17 +0000

    while

--- index1.js
+++ index1.js
@@ -17,7 +17,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/test');
 // }
 // exibeProdutos()
 
-async function main() {
+// async function main() {
     const Produto = mongoose.model('Produto',
     {
         codigo: Number,
@@ -25,24 +25,44 @@ async function main() {
         preco:Number
     })
 
-    // Produto.create({
-    //     codigo:33,
-    //     nome:'calca Balão',
-    //     preco:67,
-    // })
+//     // Produto.create({
+//     //     codigo:33,
+//     //     nome:'calca Balão',
+//     //     preco:67,
+//     // })
 
 
-    await Produto.findOneAndUpdate(
-        {preco:89},
-        {preco:120}
-        )
+//     await Produto.findOneAndUpdate(
+//         {preco:89},
+//         {preco:120}
+//         )
 
-    //     await Produto.findOneAndDelete({preco:59})
+//     //     await Produto.findOneAndDelete({preco:59})
 
-        console.log(await Produto.find())
+//         console.log(await Produto.find())
 
 
+// }
+
+// main()
+
+const produtos = [
+    {nome: "monitor", codigo: 1001, preco: 300},
+    {nome: "lorem", codigo: 12101, preco: 301320},
+    {nome: "dor", codigo: 1011201, preco: 30110}
+    
+]
+
+async function criaNovoProduto(args){
+    const tamanho = array.length
+    let contador=0
+    while(contador <= tamanho){ 
+        await Produto.create(args[contador])
+        contador++
+    }
 }
 
-main()
+// criaProdutos(produto:"monitor", codigo:1001, preco:300, 0){
+//     await Produto
+// }
 
